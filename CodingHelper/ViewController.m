4d97ea9fa4d0dea432281b7f9e0a5f570887ab25
徐伟博 <1741940001@qq.com>
2019-12-29 17:47:02 +0800

    comment

--- CodingHelper/ViewController.m
+++ CodingHelper/ViewController.m
@@ -21,6 +21,12 @@
     
     UIView *red = viewWithFrameAndBackgroundColor(CGRectMake(100, 100, 100, 100), UIColor.redColor);
     [self.view addSubview:red];
+    
+    // https://segmentfault.com/a/1190000013980664
+    // 0.代码就绪, 打tag
+    // 1.创建.Podspec文件
+    // 2.检验.Podspec文件
+    // 3.推送到远程的cocoapods, Done!
 }
 
 @end
